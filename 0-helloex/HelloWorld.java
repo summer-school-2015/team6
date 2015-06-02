7e8a176a2c075677207c57327507260f8b30d54e
Mikhail Yutman <myutman@gmail.com>
2015-06-02 11:12:33 +0300

    hello world 2 files

---
+++ 0-helloex/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		new Printer().print("Hello World!");
+	}
+}
---
+++ 0-helloex/Printer.java
@@ -0,0 +1,5 @@
+public class Printer{
+	public static void print(String s){
+		System.out.println(s);
+	}
+}
